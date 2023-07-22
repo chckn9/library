a55c9be0a5cc017ad3d237743d5f920e52611815
chckn9 <thomasyeoh9@gmail.com>
2023-07-22 16:08:27 +0800

    Display placeholder books

--- script.js
+++ script.js
@@ -2,20 +2,19 @@
 let myLibrary = [];
 
 // Constructor
-function Book(name, author, pages, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBookToLibrary(name, author, pages, read) {
-    let newBook = new Book(name, author, pages, read);
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
 // Add placeholder books
-// 1
 addBookToLibrary('7 Habits', 'Stephen Covey', '432', 'Yes');
 addBookToLibrary('Steve Jobs Biography', 'Isaac Walterson', '656', 'Yes');
 addBookToLibrary('Good Strategy Bad Strategy', 'Richard Rumelt', '336', 'No');
