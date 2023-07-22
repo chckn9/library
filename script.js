1441db71d1bcc5869226756d045b1089bdeeb192
chckn9 <thomasyeoh9@gmail.com>
2023-07-22 15:58:00 +0800

    Add placeholder books to array

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function addBookToLibrary(name, author, pages, read) {
     let newBook = new Book(name, author, pages, read);
     myLibrary.push(newBook);
 }
+
+// Add placeholder books
+// 1
+addBookToLibrary('7 Habits', 'Stephen Covey', '432', 'Yes');
+addBookToLibrary('Steve Jobs Biography', 'Isaac Walterson', '656', 'Yes');
+addBookToLibrary('Good Strategy Bad Strategy', 'Richard Rumelt', '336', 'No');
